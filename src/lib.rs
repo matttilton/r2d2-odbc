427d529165447f37a386532c7eadfd27147146f9
Matthew Tilton <Matthew.Tilton@bontsoftware.com>
2019-08-22 09:53:13 -0500

    removed massive comment block

--- src/lib.rs
+++ src/lib.rs
@@ -71,44 +71,6 @@ impl ODBCConnectionManager {
     }
 }
 
-/// An `r2d2::ManageConnection` for ODBC connections.
-///
-/// ## Example
-///
-/// ```rust,no_run
-/// extern crate r2d2;
-/// extern crate r2d2_odbc;
-/// extern crate odbc;
-///
-/// use std::thread;
-/// use r2d2_odbc::ODBCConnectionManager;
-/// use odbc::*;
-///
-/// fn main() {
-///     let manager = ODBCConnectionManager::new("DSN=PostgreSQL");
-///     let pool = r2d2::Pool::new(manager).unwrap();
-///
-///     let mut children = vec![];
-///     for i in 0..10i32 {
-///         let pool = pool.clone();
-///         children.push(thread::spawn(move || {
-///             let pool_conn = pool.get().unwrap();
-///             let conn = pool_conn.raw();
-///             let stmt = Statement::with_parent(&conn).unwrap();
-///             if let Data(mut stmt) = stmt.exec_direct("SELECT version()").unwrap() {
-///                 while let Some(mut cursor) = stmt.fetch().unwrap() {
-///                     if let Some(val) = cursor.get_data::<&str>(0).unwrap() {
-///                         print!("THREAD {} {}", i, val);
-///                     }
-///                 }
-///             }
-///         }));
-///     }
-///     for child in children {
-///         let _ = child.join();
-///     }
-/// }
-/// ```
 impl r2d2::ManageConnection for ODBCConnectionManager {
     type Connection = ODBCConnection<'static>;
     type Error = ODBCError;
